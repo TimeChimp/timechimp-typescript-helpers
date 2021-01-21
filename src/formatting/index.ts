35af9f0d930fcd11449db241e38790e99fa54ea7
wouterbaltus <wouterbaltus>
2021-01-21 15:51:56 +0100

    Restructured index.ts files

---
+++ src/formatting/index.ts
@@ -0,0 +1,7 @@
+export { formatCurrency } from './format-currency';
+export { unformatCurrency } from './unformat-currency';
+export { formatFloat } from './format-float';
+export { formatNumber } from './format-number';
+export { formatDistance } from './format-distance';
+export { formatTime } from './format-time';
+export { formatUppercase } from './format-uppercase';
