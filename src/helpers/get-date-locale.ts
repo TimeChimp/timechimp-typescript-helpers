ae2cbf9403d67cf4643e3545633f7a1f56c2cad1
Hidde Ottens <hidde95@gmail.com>
2021-02-17 11:56:25 +0100

    fix: add missing en-gb locale import

--- src/helpers/get-date-locale.ts
+++ src/helpers/get-date-locale.ts
@@ -1,11 +1,12 @@
 import { SupportedLocale } from '../common/models/types/supported-locales';
-import { enUS, nl } from 'date-fns/locale';
+import { enUS, enGB, nl } from 'date-fns/locale';
 
 export const getDateLocale = (locale: SupportedLocale) => {
-  switch (locale) {
-    case 'nl-NL':
-      return nl;
-    case 'en-US':
-      return enUS;
-  }
+  const locales = {
+    'nl-NL': nl,
+    'en-US': enUS,
+    'en-GB': enGB,
+  };
+
+  return locales[locale];
 };
