6a25fa7719aead75e05aa6b02fb65cb4628f2c68
Hidde Ottens <hidde95@gmail.com>
2022-05-02 13:24:12 +0200

    fix: add missing timer func export (#97)
    * feat: add time format functions
    
    * fix: add missing export

--- src/formatting/index.ts
+++ src/formatting/index.ts
@@ -5,3 +5,4 @@ export { formatNumber } from './format-number';
 export { formatDistance } from './format-distance';
 export { formatDuration } from './format-duration';
 export { formatUppercase } from './format-uppercase';
+export * from './format-time';
