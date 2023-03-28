e75a12bc9252a3518b40a69ec8bce41385f81147
Hidde Ottens <hidde95@gmail.com>
2023-03-28 17:03:56 +0200

    fix: duration seconds rounding (#117)
    * fix: seconds rounding issue
    
    * fix: format time unit test
    
    * chore: fix duration issues

--- src/formatting/format-duration.ts
+++ src/formatting/format-duration.ts
@@ -2,12 +2,15 @@ import { formatNumber } from './format-number';
 import { DurationFormat } from '../common/models/types/duration-format';
 import { NumberFormat } from '../common/models/types/number-format';
 
+const SECONDS_IN_HOUR = 3600;
+const SECONDS_IN_MINUTE = 60;
+
 export const formatDuration = (
   seconds: number,
   format: DurationFormat = 'HH:mm',
   decimalFormat: NumberFormat = NumberFormat.Dot
 ) => {
-  if (isNaN(seconds)) {
+  if (typeof seconds !== 'number' || isNaN(seconds)) {
     seconds = 0;
   }
 
@@ -20,27 +23,25 @@ export const formatDuration = (
   }
 
   if (format === 'HH:mm:ss' || format === 'HH:mm') {
-    const hour = Math.floor(seconds / 3600);
-    const minute = Math.round((seconds - hour * 3600) / 60);
+    const hour = Math.floor(seconds / SECONDS_IN_HOUR);
+    const minute = seconds / SECONDS_IN_MINUTE - hour * 60;
+    const roundedMinute = Math.floor(minute);
 
     const formatedHour = hour < 10 ? `0${hour}` : hour;
+    const formatedMinute =
+      roundedMinute < 10 ? `0${roundedMinute}` : roundedMinute;
 
-    if (minute < 10) {
-      time = `${formatedHour}:0${minute}`;
-    } else {
-      time = `${formatedHour}:${minute}`;
-    }
+    time = `${formatedHour}:${formatedMinute}`;
 
     if (format === 'HH:mm:ss') {
-      const second = Math.floor(seconds - hour * 3600 - minute * 60);
-      if (second < 10) {
-        time = `${time}:0${second}`;
-      } else {
-        time = `${time}:${second}`;
-      }
+      const remainingSeconds =
+        seconds - hour * SECONDS_IN_HOUR - roundedMinute * SECONDS_IN_MINUTE;
+      const formatedSeconds =
+        remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds;
+      time = `${time}:${formatedSeconds}`;
     }
   } else if (format === 'decimal') {
-    time = formatNumber(seconds / 3600, 2, decimalFormat);
+    time = formatNumber(seconds / SECONDS_IN_HOUR, 2, decimalFormat);
   }
 
   if (isNegative) {
