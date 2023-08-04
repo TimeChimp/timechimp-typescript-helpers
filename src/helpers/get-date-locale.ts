2e36694d25cbf932ddca8832f4097de9a8661138
OksanaTC <o.kukushkina@timechimp.com>
2023-08-04 14:12:25 +0200

    Fix imports

--- src/helpers/get-date-locale.ts
+++ src/helpers/get-date-locale.ts
@@ -2,9 +2,9 @@ import { SupportedLocale } from '../common/models/types/supported-locales';
 import nl from 'date-fns/locale/nl';
 import enGB from 'date-fns/locale/en-GB';
 import de from 'date-fns/locale/de';
-import fr from 'data-fns/locale/fr';
+import fr from 'date-fns/locale/fr';
 import pl from 'date-fns/locale/pl';
-import es from 'data-fns/locale/es';
+import es from 'date-fns/locale/es';
 
 export const getDateLocale = (locale: SupportedLocale) => {
   const locales = {
